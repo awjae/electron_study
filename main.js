9e1b822aab659976cd3cea308b42d280ec3d6889
AW <djwotmd@gmail.com>
2023-07-17 17:46:34 +0900

    [ADD] : electron 환경 설정중

--- main.js
+++ main.js
@@ -1,24 +1,61 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
- 
+const { app, BrowserWindow, electron } = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
+
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 640,
-        height: 480,
-        webPreferences: { preload: path.join(__dirname, 'preload.js') }
-    });
- 
-    win.loadFile('index.html');
-};
- 
+  const WEB_FOLDER = 'web'
+  const PROTOCOL = 'file'
+
+  electron.protocol.interceptFileProtocol(PROTOCOL, (request, callback) => {
+    // // Strip protocol
+    let url = request.url.substr(PROTOCOL.length + 1)
+
+    // Build complete path for node require function
+    url = path.join(__dirname, WEB_FOLDER, url)
+
+    // Replace backslashes by forward slashes (windows)
+    // url = url.replace(/\\/g, '/');
+    url = path.normalize(url)
+
+    console.log(url)
+    callback({ path: url })
+  })
+
+  const win = new BrowserWindow({
+    width: 640,
+    height: 480,
+    // frame: true,
+    // transparent: true,
+    // backgroundColor: 'rgba(0, 0, 0, .8)',
+    // webPreferences: { preload: path.join(__dirname, 'preload.js') },
+    nodeIntegration: true,
+    contextIsolation: false,
+  })
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  // win.loadFile('index.html');
+  // win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
+  mainWindow.loadURL(
+    url.format({
+      pathname: 'index.html',
+      protocol: PROTOCOL + ':',
+      slashes: true,
+    }),
+  )
+  win.loadFile('./build/index.html')
+}
+
 app.whenReady().then(() => {
-    createWindow();
- 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-});
- 
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-});
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['main.js'],
+  },
+  build: {
+    outDir: 'build',
+  },
+  /*server: {
+    host: '10.80.17.120',
+    proxy: {
+      '/api': {
+        target: 'http://172.27.2.222:8080/',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+        ws: true
+      }
+    }
+  }*/
+})
