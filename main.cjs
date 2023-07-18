e2ef5a57933856472ee8a8592f50c0547506641a
AW <djwotmd@gmail.com>
2023-07-18 10:28:12 +0900

    [ADD] : Vite, Electron 모듈방식 호환 에러로 인한 환경 수정

--- main.cjs
+++ main.cjs
@@ -1,25 +1,11 @@
-const { app, BrowserWindow, protocol } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const isDev = require('electron-is-dev')
 const path = require('path')
+const url = require('url')
 
 const createWindow = () => {
   const WEB_FOLDER = 'web'
   const PROTOCOL = 'file'
-
-  protocol.interceptFileProtocol(PROTOCOL, (request, callback) => {
-    // // Strip protocol
-    let url = request.url.substr(PROTOCOL.length + 1)
-
-    // Build complete path for node require function
-    url = path.join(__dirname, WEB_FOLDER, url)
-
-    // Replace backslashes by forward slashes (windows)
-    // url = url.replace(/\\/g, '/');
-    url = path.normalize(url)
-
-    console.log(url)
-    callback({ path: url })
-  })
 
   const win = new BrowserWindow({
     width: 640,
@@ -28,24 +14,38 @@ const createWindow = () => {
     // transparent: true,
     // backgroundColor: 'rgba(0, 0, 0, .8)',
     // webPreferences: { preload: path.join(__dirname, 'preload.js') },
-    nodeIntegration: true,
-    contextIsolation: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      worldSafeExecuteJavaScript: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, 'preload.js'),
+    },
   })
 
   if (isDev) {
-    mainWindow.webContents.openDevTools()
+    win.webContents.openDevTools()
   }
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, '/build/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  })
+  win.loadURL(startUrl)
+
+  console.log(win.webContents.getURL())
 
   // win.loadFile('index.html');
   // win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
-  mainWindow.loadURL(
-    url.format({
-      pathname: 'index.html',
-      protocol: PROTOCOL + ':',
-      slashes: true,
-    }),
-  )
-  win.loadFile('./build/index.html')
+  // win.loadURL(
+  //   url.format({
+  //     pathname: path.join(__dirname, '/../build/index.html'),
+  //     protocol: PROTOCOL + ':',
+  //     slashes: true,
+  //   }),
+  // )
+  // win.loadFile('./build/index.html')
 }
 
 app.whenReady().then(() => {
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   optimizeDeps: {
     exclude: ['main.js'],
   },
   build: {
     outDir: 'build',
+    // rollupOptions: {
+    //   output: {
+    //     format: 'cjs'
+    //   }
+    // }
   },
   /*server: {
     host: '10.80.17.120',
