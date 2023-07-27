1004bdab4b761ba847b29ed6c0d33885242d3b53
AW <djwotmd@gmail.com>
2023-07-27 23:56:22 +0900

    [ADD] : 기술 스택 추가 및 인스톨

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   optimizeDeps: {
-    exclude: ['main.js'],
+    exclude: ['main.cjs', 'server'],
   },
   build: {
     outDir: 'build',
