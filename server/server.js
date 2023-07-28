e731eb3abd4267a93758221dea73f9bd0ad0bfad
AW <djwotmd@gmail.com>
2023-07-28 16:26:57 +0900

    [ADD] : express-graphql 추가

--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema } from 'graphql'
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+var root = { hello: () => 'Hello world!' }
 
 const app = express()
 
@@ -6,4 +16,13 @@ app.get('/', (req, res) => {
   res.send('hello')
 })
 
-app.listen(80)
+app.get(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+)
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'))
