8c41805816ef20d6abbab959a5dc90a1c519f3d0
AW <49174221+awjae@users.noreply.github.com>
2023-08-04 15:25:16 +0900

    [CLN] : 모듈 import 버그 처리 및 코드 캡슐화

---
+++ server/pgPool.js
@@ -0,0 +1,53 @@
+
+import pg from "pg";
+const { Pool } = pg;
+import graphql from 'graphql';
+const { buildSchema } = graphql;
+
+export default class PgPool {
+  constructor() {
+    this.pool = new Pool({
+      host: 'svc.sel3.cloudtype.app',
+      port: 30641,
+      database: 'trip_genie',
+      user: 'admin',
+      password: 'admin',
+    })
+    this.userSchema = buildSchema(`
+      type User {
+        id: Int
+        name: String
+        age: Int
+        email: String
+      }
+    
+      type Query {
+        getUser(id: Int!): User
+        getUsers: [User]
+      }
+    `)
+    this.userRoot = {
+      getUser: async ({ id }) => {
+        const query = 'SELECT * FROM users WHERE id = $1'
+        const params = [id]
+        const result = await runQuery(query, params)
+        return result[0]
+      },
+      getUsers: async () => {
+        const query = 'SELECT * FROM users'
+        const result = await runQuery(query)
+        return result
+      },
+    }
+  }
+  async runQuery(query, params) {
+    try {
+      const client = await this.pool.connect()
+      const result = await client.query(query, params)
+      client.release()
+      return result.rows
+    } catch (error) {
+      throw new Error(`Error executing query: ${error}`)
+    }
+  }
+}
